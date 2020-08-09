f9c25a12a70cef2c4f91e674787127225c7f3eb4
HongKyungWon <hkw7984@naver.com>
2020-08-09 20:20:19 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+  'name' : '홍길동',
+  'birthday' : '950922',
+  'gender' : '남자',
+  'job' : '대학생'
+}
 
 class App extends React.Component {
   render() {
     return (
-      <div className = "gray-background">
-        <img src = {logo} lat = "log"></img>
-        <h2>Let's develop management system</h2>
-      </div>
+      <Customer
+         name = {customer.name}
+         birthday = {customer.birthday}
+         gender = {customer.gender}
+         job = {customer.job}
+      />
     )
   }
 }
